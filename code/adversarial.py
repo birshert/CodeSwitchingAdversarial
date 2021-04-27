780b8c4b97f7ad77ec15af7cdd136e3e2de8231f
birshert <birshertalexey21@gmail.com>
2021-04-27 17:11:34 +0300

    Alignment attack ready

--- code/adversarial.py
+++ code/adversarial.py
@@ -5,6 +5,7 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from word2word import Word2word
+from collections import defaultdict
 
 from dataset import (
     CustomDataset,
@@ -18,6 +19,8 @@ from utils import (
     tokenize_and_preserve_labels,
 )
 
+from time import time
+
 
 class BaseAdversarial:
 
@@ -29,9 +32,11 @@ class BaseAdversarial:
         self.config = load_config()
 
         self.model = model_mapping[self.config['model_name']](config=self.config)
+        self.model.eval()
         self.model.to(self.device, non_blocking=True)
 
         self.base_language = base_language
+        self.num_examples = 1
 
     def get_candidates(self, *args, **kwargs):
         raise NotImplementedError
@@ -45,25 +50,28 @@ class BaseAdversarial:
 
         data = []
 
+        starting_time = time()
+
         for idx, row in tqdm(test.iterrows(), desc='GENERATING ADVERSARIAL EXAMPLES', total=len(test)):
             x = row['utterance']
             y_slots = row['slot_labels']
             y_intent = row['intent']
 
-            tokens, slot_labels = tokenize_and_preserve_labels(
-                self.model.tokenizer,
-                ' '.join(self.attack(x, y_slots, y_intent)),
-                y_slots,
-                self.slot2idx
-            )
-
-            data.append(
-                (
-                    tokens,
-                    slot_labels,
-                    self.intent2idx.get(y_intent, self.intent2idx['UNK'])
+            for _ in range(self.num_examples):
+                tokens, slot_labels = tokenize_and_preserve_labels(
+                    self.model.tokenizer,
+                    ' '.join(self.attack(x, y_slots, y_intent)),
+                    y_slots,
+                    self.slot2idx
                 )
-            )
+
+                data.append(
+                    (
+                        tokens,
+                        slot_labels,
+                        self.intent2idx.get(y_intent, self.intent2idx['UNK'])
+                    )
+                )
 
         dataset = CustomDataset(data, self.model.tokenizer, self.slot2idx)
         loader = DataLoader(
@@ -73,11 +81,11 @@ class BaseAdversarial:
 
         results = evaluate(
             self.model, loader, fp_16=True,
-            epoch=0, num_epoches=1,
             slot2idx=self.slot2idx, idx2slot=self.idx2slot
         )
 
         results['loss'] = results.pop('loss [VALID]')
+        results['time'] = time() - starting_time
 
         return results
 
@@ -124,6 +132,8 @@ class Pacifist(BaseAdversarial):
     def __init__(self, base_language: str = 'en'):
         super().__init__(base_language)
 
+        self.num_examples = 1
+
     def get_candidates(self, x, y_slots, y_intent, pos):
         pass
 
@@ -131,7 +141,7 @@ class Pacifist(BaseAdversarial):
         return x.split()
 
 
-class Adversarial1(BaseAdversarial):
+class AdversarialWordLevel(BaseAdversarial):
     """
     Simple adversarial attack based on changing tokens to their translations in SET of target languages.
     Token's translation is chosen in order to maximize model's loss.
@@ -149,6 +159,8 @@ class Adversarial1(BaseAdversarial):
 
         self.translators = {lang: Word2word('en', lang) for lang in languages}
 
+        self.rng = np.random.default_rng()
+
     def attack(self, x, y_slots, y_intent):
         if isinstance(x, str):
             x = x.split()
@@ -158,7 +170,7 @@ class Adversarial1(BaseAdversarial):
 
         current_loss = self.calculate_loss(x, y_slots, y_intent)
 
-        for pos in np.random.permutation(len(x)):
+        for pos in self.rng.permutation(len(x)):
             candidates, losses = self.get_candidates(x, y_slots, y_intent, pos)
 
             if candidates and current_loss < np.max(losses):
@@ -174,7 +186,7 @@ class Adversarial1(BaseAdversarial):
 
         for lang in self.languages:
             try:
-                token = np.random.choice(self.translators[lang](x[pos], n_best=3))
+                token = np.random.choice(self.translators[lang](x[pos].lower(), n_best=3))
                 xc[pos] = token
 
                 losses.append(self.calculate_loss(xc, y_slots, y_intent))
@@ -185,58 +197,128 @@ class Adversarial1(BaseAdversarial):
         return candidates, losses
 
 
-class Adversarial2(Adversarial1):
-    """
-    Simple adversarial attack based on changing tokens to their translations in ONE target language.
-    Token's translation is chosen in order to maximize model's loss.
-    Translations are generated with dictionaries from word2word library.
-    """
+def mapping_alignments(lines, data):
+    mapping = {}
+
+    for idx, line in enumerate(lines):
+        mapping[idx] = defaultdict(list)
+        text = data[idx].strip().split()
+
+        for elem in line.split():
+            key, value = map(int, elem.split('-'))
+            mapping[idx][key].append(text[value])
+
+        mapping[idx] = dict(mapping[idx])
+
+    return mapping
+
+
+class AdversarialAlignments(BaseAdversarial):
 
     def __init__(self, base_language: str = 'en', languages: list = None):
-        super().__init__(base_language, languages)
+        super().__init__(base_language)
 
-        self.attack_language = None
+        if languages is None:
+            languages = self.config['languages']
+            languages.remove(self.base_language)
 
-    def attack(self, x, y_slots, y_intent, language=None):
-        if language is None:
-            self.attack_language = np.random.choice(self.languages)
-        elif language in self.languages:
-            self.attack_language = language
-        else:
-            raise ValueError(f'Language {language} is not listed as one of the languages used.')
+        self.languages = languages
 
-        return super().attack(x, y_slots, y_intent)
+        self.alignments = {}
 
-    def get_candidates(self, x, y_slots, y_intent, pos):
+        for language in self.languages:
+            with open(f'data/alignment/{self.base_language}_{language}.out') as f:
+                self.alignments[language] = mapping_alignments(f.readlines(), read_atis('test', [language])['utterance'])
+
+        self.rng = np.random.default_rng()
+
+    def attack(self, x, y_slots, y_intent, alignments):
+        if isinstance(x, str):
+            x = x.split()
+
+        if isinstance(y_slots, str):
+            y_slots = y_slots.split()
+
+        current_loss = self.calculate_loss(x, y_slots, y_intent)
+
+        for pos in self.rng.permutation(len(x)):
+            candidates, losses = self.get_candidates(x, y_slots, y_intent, pos, alignments)
+
+            if candidates and current_loss < np.max(losses):
+                current_loss = np.max(losses)
+                x[pos] = candidates[np.argmax(losses)]
+
+        return x
+
+    def get_candidates(self, x, y_slots, y_intent, pos, alignments):
         xc = deepcopy(x)
+        y_slots_c = deepcopy(y_slots)
 
         candidates, losses = [], []
 
-        try:
-            token = np.random.choice(self.translators[self.attack_language](x[pos], n_best=3))
-            xc[pos] = token
+        for lang in self.languages:
+            try:
+                tokens = alignments[lang][pos]
 
-            losses.append(self.calculate_loss(xc, y_slots, y_intent))
-            candidates.append(token)
-        except KeyError:
-            pass
+                if len(tokens) > 1:
+                    if y_slots[pos].startswith('B'):
+                        new_slot_label = 'I' + y_slots[pos][1:]
+                        y_slots_c[pos] = y_slots[pos] + ' '.join(new_slot_label for _ in range(len(tokens) - 1))
+                    else:
+                        y_slots_c[pos] = ' '.join(y_slots[pos] for _ in range(len(tokens)))
+
+                xc[pos] = ' '.join(tokens)
+
+                losses.append(self.calculate_loss(' '.join(xc), ' '.join(y_slots_c), y_intent))
+                candidates.append(' '.join(tokens))
+            except KeyError:
+                pass
 
         return candidates, losses
 
+    @torch.no_grad()
+    def attack_test(self):
+        test = read_atis('test', [self.base_language])
 
-# class Adversarial3(BaseAdversarial):
-#
-#     def __init__(self, base_language: str = 'en', languages: list = None):
-#         super().__init__(base_language)
-#
-#         if languages is None:
-#             languages = self.config['languages']
-#             languages.remove(self.base_language)
-#
-#         self.languages = languages
-#
-#         self.alignments =
-#
-#     def attack(self, *args, **kwargs):
-#
-#     def get_candidates(self, *args, **kwargs):
+        data = []
+
+        starting_time = time()
+
+        for idx, row in tqdm(test.iterrows(), desc='GENERATING ADVERSARIAL EXAMPLES', total=len(test)):
+            x = row['utterance']
+            y_slots = row['slot_labels']
+            y_intent = row['intent']
+
+            alignments = {language: self.alignments[language][idx] for language in self.languages}
+
+            for _ in range(self.num_examples):
+                tokens, slot_labels = tokenize_and_preserve_labels(
+                    self.model.tokenizer,
+                    ' '.join(self.attack(x, y_slots, y_intent, alignments)),
+                    y_slots,
+                    self.slot2idx
+                )
+
+                data.append(
+                    (
+                        tokens,
+                        slot_labels,
+                        self.intent2idx.get(y_intent, self.intent2idx['UNK'])
+                    )
+                )
+
+        dataset = CustomDataset(data, self.model.tokenizer, self.slot2idx)
+        loader = DataLoader(
+            dataset, shuffle=True, batch_size=8,
+            pin_memory=True, drop_last=False, collate_fn=dataset.collate_fn
+        )
+
+        results = evaluate(
+            self.model, loader, fp_16=True,
+            slot2idx=self.slot2idx, idx2slot=self.idx2slot
+        )
+
+        results['loss'] = results.pop('loss [VALID]')
+        results['time'] = time() - starting_time
+
+        return results
